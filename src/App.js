25d59acb21261b3b226d8ab4301a28a1ca27faf4
roobana <roobanachinraj@gmail.com>
2024-06-05 19:28:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import "./styles.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import MoviesGrid from "./components/MoviesGrid";
+import Watchlist from "./components/Watchlist";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <Header></Header>
+        <Router>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/Watchlist">Watchlist</Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path="/" element={<MoviesGrid />}></Route>
+            <Route path="/Watchlist" element={<Watchlist />}></Route>
+          </Routes>
+        </Router>
+      </div>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/components/Watchlist.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "../styles.css";
+export default function Watchlist() {
+  return (
+    <div>
+      <h1 className="title">Your Watchlist</h1>
+    </div>
+  );
+}
