d652b4cd90f764fd7137f327983427d3aecc01ad
roobana <roobanachinraj@gmail.com>
2024-06-05 21:47:44 +0530

    toggle is added

--- src/components/Watchlist.js
+++ src/components/Watchlist.js
@@ -1,9 +1,23 @@
 import React from "react";
 import "../styles.css";
-export default function Watchlist() {
+import MovieCard from "./MovieCard";
+export default function Watchlist({ movies, watchlist, toggleWatchlist }) {
   return (
     <div>
       <h1 className="title">Your Watchlist</h1>
+      <div className="watchlist">
+        {watchlist.map((id) => {
+          const movie = movies.find((movie) => movie.id === id);
+          return (
+            <MovieCard
+              key={id}
+              movie={movie}
+              toggleWatchlist={toggleWatchlist}
+              isWatchlisted={true}
+            ></MovieCard>
+          );
+        })}
+      </div>
     </div>
   );
 }
